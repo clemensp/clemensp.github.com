37b77af89133b7f72694d451e2ddefcc740a5ee0
Clemens Park <clemensp@nulogy.com>
2019-08-26 08:35:17 -0400

    More config updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `CP's software journey`,
+        short_name: `CP's software journey`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
